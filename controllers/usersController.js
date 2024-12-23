1a9806d8e55528fb2f9cb44e11f0ef1999f06193
asimov <seyranmustafa4@gmail.com>
2024-12-23 19:37:01 +0300

    p

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -96,7 +96,6 @@ const updateUser = asyncHandler(async (req, res) => {
 // @access Private
 const deleteUser = asyncHandler(async (req, res) => {
     const { id } = req.body
-
     // Confirm data
     if (!id) {
         return res.status(400).json({ message: 'User ID Required' })
